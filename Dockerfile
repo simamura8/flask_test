4756c8bcccf9e15060844697fbf89106a4b5abbb
simamura8 <imaron0812@gmail.com>
2023-01-14 14:31:26 +0900

    base templateの作成

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,7 @@ RUN apt install python3 -y
 RUN apt install python3-pip -y
 RUN pip3 install flask
 RUN mkdir /app
+
+# RUN apt install git
+# RUN git clone https://github.com/simamura8/flask_test.git
+
--- app/app.py
+++ app/app.py
@@ -2,6 +2,7 @@
 
 # flaskクラスをインポート
 from flask import Flask
+from flask import render_template
 
 # インスタンス化
 # __name__：importされてるときは、import名。mainのときは"__main__"
@@ -11,9 +12,16 @@ app = Flask(__name__)
 # 検索されたら、index関数を実行。
 @app.route('/')
 def index():
-    return "<h1>Hello world!<h1>"
+    # メソッドにtemplatesディレクトリのファイル名を指定してわたす
+    return render_template('index.html')
+
+# test
+@app.route('/japan/<city>')
+# japan/<引数>　でもらい、その引数をhello.htmlにわたす。
+def hello(city):
+    return render_template('hello.html', city=city)
 
 if __name__ == "__main__":
     # サーバーを立ち上げる。(bashでflask run するのと同じ)
     # appインスタンスのrunメソッドを使用。
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=5000, debug=True)
